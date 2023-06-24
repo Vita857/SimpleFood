147d0c5ef7ca2e181477caa13102450ff4175909
Vita857 <kovalvita9@gmail.com>
2023-06-24 09:39:30 +0300

    added main

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
+window.onscroll = function showHeader(){
+
+  var header = document.querySelector('.header');
+  
+  if(window.pageYOffset > 20) {
+    header.classList.add('header--fixed');
+  } else{
+    header.classList.remove('header--fixed');
+  }
+  
+  };
+
+
 $(function(){
   
 });
+
+const mixer = mixitup('.category__menu');
